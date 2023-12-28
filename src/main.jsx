92de8417ee7b5bcfc950c9b38805f57e04214b49
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-12-28 20:23:58 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./app/Store.js";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>
 );
