7bfc7597ea4f7184ccb444cb475ab0d46b38e455
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2024-01-12 11:56:03 +0500

    updated

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -6,6 +6,7 @@ import * as formik from "formik";
 import * as yup from "yup";
 import InputGroup from "react-bootstrap/InputGroup";
 import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
 
 const Auth = () => {
   const { Formik } = formik;
@@ -16,59 +17,63 @@ const Auth = () => {
   });
   return (
     <>
-      <Formik
-        validationSchema={schema}
-        onSubmit={console.log}
-        initialValues={{
-          email: "",
-          password: "",
-        }}
-      >
-        {({ handleSubmit, handleChange, values, touched, errors }) => (
-          <Form noValidate onSubmit={handleSubmit}>
-            <Row className="mb-3">
-              <Form.Group as={Col} md="6" controlId="Email">
-                <Form.Label>Email</Form.Label>
-                <InputGroup hasValidation>
-                  <Form.Control
-                    type="email"
-                    placeholder=""
-                    aria-describedby="inputGroupEmail"
-                    name="email"
-                    value={values.email}
-                    onChange={handleChange}
-                    isInvalid={!!errors.email}
-                    pattern="/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/"
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    {errors.email}
-                  </Form.Control.Feedback>
-                </InputGroup>
-              </Form.Group>
+      <Card>
+        <Card.Body>
+          <Formik
+            validationSchema={schema}
+            onSubmit={console.log}
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+          >
+            {({ handleSubmit, handleChange, values, touched, errors }) => (
+              <Form noValidate onSubmit={handleSubmit}>
+                <Row className="mb-3">
+                  <Form.Group as={Col} md="6" controlId="Email">
+                    <Form.Label>Email</Form.Label>
+                    <InputGroup hasValidation>
+                      <Form.Control
+                        type="email"
+                        placeholder=""
+                        aria-describedby="inputGroupEmail"
+                        name="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        isInvalid={!!errors.email}
+                        pattern="/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/"
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        {errors.email}
+                      </Form.Control.Feedback>
+                    </InputGroup>
+                  </Form.Group>
 
-              <Form.Group as={Col} md="6" controlId="Password">
-                <Form.Label>Password</Form.Label>
-                <InputGroup hasValidation>
-                  <Form.Control
-                    type="password"
-                    placeholder=""
-                    aria-describedby="inputGroupPassword"
-                    name="password"
-                    value={values.password}
-                    onChange={handleChange}
-                    isInvalid={!!errors.password}
-                    pattern=" /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/"
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    {errors.password}
-                  </Form.Control.Feedback>
-                </InputGroup>
-              </Form.Group>
-            </Row>
-            <Button type="submit">Submit form</Button>
-          </Form>
-        )}
-      </Formik>
+                  <Form.Group as={Col} md="6" controlId="Password">
+                    <Form.Label>Password</Form.Label>
+                    <InputGroup hasValidation>
+                      <Form.Control
+                        type="password"
+                        placeholder=""
+                        aria-describedby="inputGroupPassword"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        isInvalid={!!errors.password}
+                        pattern=" /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/"
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        {errors.password}
+                      </Form.Control.Feedback>
+                    </InputGroup>
+                  </Form.Group>
+                </Row>
+                <Button type="submit">Submit form</Button>
+              </Form>
+            )}
+          </Formik>
+        </Card.Body>
+      </Card>
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import Auth from "../components/Auth";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useState } from "react";
 
 const Home = () => {
+  const [arr,setArr]=useState([1,2,3,4])
   return (
     <>
-      <Container>
-        <Auth />
+    <Container>
+        <Row className="d-flex justify-content-center align-items-center p-md-5">
+          <Col lg={6}>
+            <Auth />
+          </Col>
+        </Row>
+        <Row xs={1} md={2} lg={3} xl={3} xxl={4}>
+         {
+          arr.map((ele,i)=>{
+            return (
+              <Col key={i} className="bg-primary m-2">
+                <h1>{ele}</h1>
+              </Col>
+            );
+          })
+         }
+        </Row>
       </Container>
     </>
   );
