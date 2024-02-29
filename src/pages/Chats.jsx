2e3b02484142ed5b9c4813fea3a00e0fdccd3816
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2024-02-29 20:11:06 +0500

    updated

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import ChatList from "../components/Chat/ChatList";
+import ChatBox from "../components/Chat/ChatBox";
+import ChatInfo from "../components/Chat/ChatInfo";
 const Chats = () => {
   return (
-   <>
-    <div style={{width:"100%"}}>
-      
-    </div>
-   </>
-  )
-}
+    <>
+      <div style={{ width: "100%" }}>
+        <Container fluid>
+          <Row>
+            <Col>
+              <ChatList />
+            </Col>
+            <Col>
+              <ChatBox />
+            </Col>
+            <Col>
+              <ChatInfo />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
 
-export default Chats
+export default Chats;
