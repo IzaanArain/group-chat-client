dcca3cbbb0d03059beec46298bc886f79da862b0
izaan <izaansaquib@gmail.com>
2024-03-04 22:32:45 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
-import store from "./app/Store.js";
+import store from "./app/Store";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { PersistGate } from "redux-persist/integration/react";
-import { persistor } from "./app/Store.js";
+import { persistor } from "./app/Store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
